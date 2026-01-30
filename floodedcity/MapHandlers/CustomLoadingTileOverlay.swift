a2adb18478b9f6ce951021bf9e646611280817fe
Robert Smith <robert@inkrh.com>
2026-01-30 08:57:53 -0600

    Quick thought while I work on other things

--- floodedcity/MapHandlers/CustomLoadingTileOverlay.swift
+++ floodedcity/MapHandlers/CustomLoadingTileOverlay.swift
@@ -25,7 +25,7 @@ class CustomLoadingTileOverlay: MKTileOverlay {
             let result: (Data, URLResponse) = try await urlSession.data(from: urlToLoad)
             let mapTileData = result.0
             //      really hacky way to handle slow connections
-            usleep(500_000)
+//            usleep(500_000)
             //bug hunting
             if let httpResponse = result.1 as? HTTPURLResponse,
                !(200...299).contains(httpResponse.statusCode) {
@@ -39,8 +39,9 @@ class CustomLoadingTileOverlay: MKTileOverlay {
             print("Other error: \(error.localizedDescription)")
         }
         //temporary while I look at it - results in infinite loop if fails
-        //but is only failing once and succeeding until map is moved and
-        //the logs show this isn't even being hit
+        //but is only failing once and succeeding until map is moved? and
+        //the logs show this isn't even being hit?
+        //possible is all down to server bandwidth, refusing multiple connections before it gets to server-side logging?
         return try await loadTile(at: path)
     }
 }
