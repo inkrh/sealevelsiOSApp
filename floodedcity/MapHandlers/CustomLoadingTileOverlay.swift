fb7ec6ebc06fa1d6b595db866913d7bc0a5e2704
Robert Smith <robert@inkrh.com>
2026-01-30 08:54:10 -0600

    Pausing point while I think

--- floodedcity/MapHandlers/CustomLoadingTileOverlay.swift
+++ floodedcity/MapHandlers/CustomLoadingTileOverlay.swift
@@ -18,12 +18,29 @@ class CustomLoadingTileOverlay: MKTileOverlay {
         urlSession = URLSession(configuration: config)
     }
 
+    
     override func loadTile(at path: MKTileOverlayPath) async throws -> Data {
         let urlToLoad = url(forTilePath: path)
-        let result = try await urlSession.data(from: urlToLoad)
-        let mapTileData = result.0
-//      really hacky way to handle slow connections
-        usleep(500_000)
-        return mapTileData
+        do {
+            let result: (Data, URLResponse) = try await urlSession.data(from: urlToLoad)
+            let mapTileData = result.0
+            //      really hacky way to handle slow connections
+            usleep(500_000)
+            //bug hunting
+            if let httpResponse = result.1 as? HTTPURLResponse,
+               !(200...299).contains(httpResponse.statusCode) {
+                print("HTTP error: \(httpResponse.statusCode)")
+            }
+            
+            return mapTileData
+        } catch  let urlError as URLError {
+            print("URLError \(urlError.localizedDescription)")
+        } catch {
+            print("Other error: \(error.localizedDescription)")
+        }
+        //temporary while I look at it - results in infinite loop if fails
+        //but is only failing once and succeeding until map is moved and
+        //the logs show this isn't even being hit
+        return try await loadTile(at: path)
     }
 }
--- floodedcity/MapHandlers/MapTIleViewControllerExtension.swift
+++ floodedcity/MapHandlers/MapTIleViewControllerExtension.swift
@@ -75,11 +75,25 @@ extension ViewController {
     }
 
     func mapViewDidFailLoadingMap(_ mapView: MKMapView, withError error: Error) {
-//        print("Map failed to load with error: \(error.localizedDescription)")
+        print("Map failed to load with error: \(error.localizedDescription)")
+        loadingIndicator.stopAnimating()
+        
         //TODO: refresh button to attempt reload - failing without actual error
+        
         //Likely timeout, but server logs show all calls received succeeding, probably timing with presentation layer being visible rather than server?
         //Might be down to Apple's internal networking handling - logs show socket failure, so failing before it reaches server == timing?
-        loadingIndicator.stopAnimating()
+        //Might also be down to using a non-standard port
+        
+        //Both of these attempts fail silently
+        //1
+//        displaySeaLevelsMap()
+        //2
+//        if let overlay = self.mapView.overlays.first(where: { $0 is MKTileOverlay }) as? MKTileOverlay {
+//            if let renderer = self.mapView.renderer(for: overlay) as? MKTileOverlayRenderer {
+//                renderer.reloadData()
+//            }
+//        }
+
     }
 
 }
